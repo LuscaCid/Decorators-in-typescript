9bb8d934a475c96b73a4f99104827652124ab270
Lucas Cid <lucasfelipaaa@gmail.com>
2024-01-24 15:32:08 +0000

    finishing initial configs

---
+++ tic-tac-toe/src/components/square-x/index.tsx
@@ -0,0 +1,9 @@
+import Container from './style'
+import { FaTimes } from "react-icons/fa";
+export const SquareX = () => {
+    return (
+        <Container>
+            <FaTimes size = {20}/> 
+        </Container> 
+    )
+}
